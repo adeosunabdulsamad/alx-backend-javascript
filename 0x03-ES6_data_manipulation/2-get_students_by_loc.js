9e10b05fe5756bd6b9570ef3d367aec02498bf55
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-10-08 19:10:03 +0100

    created a function that uses the filter function of an array

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getListStudentIds(students, city) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    return students.filter((student) => student.location === city);
+}
