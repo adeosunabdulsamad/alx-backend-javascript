1b28cd977070c0f88d833c757cef4de0fa07284e
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-10-08 17:29:28 +0100

    Refactor getListStudentsIds function to use array literal syntax for student objects

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,3 @@
-export default function getListStudentsIds(arr{id, firstName, location}) {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 3, firstName: 'Serena', location: 'San Francisco' },
-    ];
+export default function getListStudentIds(arr) {
+    return arr.map((item) => item.id);
 }
