01d7de20552df416ab9890281a99a42a91f590a1
Abdulsamad Adeosun <59876143+adeosunabdulsamad@users.noreply.github.com>
2025-01-28 16:08:23 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello ALX!');
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
+
