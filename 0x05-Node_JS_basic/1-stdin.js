9137a720cfe0d6bf5fb6925f3e21140289fc94bd
Abdulsamad Adeosun <59876143+adeosunabdulsamad@users.noreply.github.com>
2025-01-28 15:45:02 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to ALX, what is your name?');
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
