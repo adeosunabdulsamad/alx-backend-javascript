f0799961258cf80661f4034aed63d6a6120a16c0
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-02-02 14:52:57 +0000

    Used hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function () {
+    let consoleSpy;
+
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        consoleSpy.restore();
+    });
+
+    it('should log the correct total for (100, 20)', function () {
+        sendPaymentRequestToApi(100, 20);
+
+        expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+        expect(consoleSpy.calledOnce).to.be.true;
+    });
+
+    it('should log the correct total for (10, 10)', function () {
+        sendPaymentRequestToApi(10, 10);
+
+        expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+        expect(consoleSpy.calledOnce).to.be.true;
+    });
+});
