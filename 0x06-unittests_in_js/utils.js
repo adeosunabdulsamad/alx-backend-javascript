a6114dfc29d2b11d02a3d6dbd83c6ee1bb15ca23
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-02-02 14:44:14 +0000

    installed sinon with npm

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+  calculateNumber: (type, a, b) => {
+    const num1 = Math.round(a);
+    const num2 = Math.round(b);
+
+    switch (type) {
+      case 'SUM':
+        return num1 + num2;
+      case 'SUBTRACT':
+        return num1 - num2;
+      case 'DIVIDE':
+        return num2 === 0 ? 'Error' : num1 / num2;
+      default:
+        throw new Error('Invalid operation type');
+    }
+  },
+};
+
+module.exports = Utils;
